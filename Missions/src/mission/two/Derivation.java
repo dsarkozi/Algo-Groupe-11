069e30585c69954e02ea4196d7669cfab32c047a
David Sarkozi <dsarko@live.be>
2013-10-10 14:40:44 +0200

    Mission 2 init

---
+++ Missions/src/mission/two/Derivation.java
@@ -0,0 +1,33 @@
+package mission.two;
+
+/**
+ * @author
+ * 
+ */
+public class Derivation implements FormalExpressionTree
+{
+
+	/**
+	 * 
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String toString()
+	{
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+	@Override
+	public FormalExpressionTree derive()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
