67d7e2eb7ce97ad2e560912e07218081b989d733
bsluysmans <benoitsluysmans@gmail.com>
2013-10-10 20:50:11 +0200

    Create LinkedRBinaryTree.java

---
+++ Missions/src/mission/two/LinkedRBinaryTree.java
@@ -0,0 +1,78 @@
+package mission.two;
+
+
+public class LinkedRBinaryTree<E> implements RBinaryTree<E> {
+    
+	protected BTNode<E> root;
+    protected int size;
+
+    public LinkedRBinaryTree() {
+            root = null;
+            size = 0;
+    }
+    
+    public LinkedRBinaryTree(BTNode<E> e) {
+            root = e;
+            size = 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+            return (root == null);
+    }
+
+    @Override
+    public int size() {
+            return size;    
+    }
+
+    @Override
+    public Position<E> root() {
+            return root;
+    }
+
+    @Override
+    public boolean isLeaf() {
+            return (root.getLeft() == null && root.getRight() == null);
+    }
+
+    @Override
+    public RBinaryTree<E> leftTree() {
+            return new LinkedRBinaryTree<E>((BTNode<E>) root.getLeft());
+    }
+
+    @Override
+    public RBinaryTree<E> rightTree() {
+            return new LinkedRBinaryTree<E>((BTNode<E>) root.getRight());
+    }
+
+    @Override
+    public void setElement(E o) {
+            root.setElement(o);
+    }
+
+    @Override
+    public void setLeft(RBinaryTree<E> tree) {
+            root.setLeft((BTNode<E>) tree.root());
+    }
+
+    @Override
+    public void setRight(RBinaryTree<E> tree) {
+            root.setRight((BTNode<E>) tree.root());
+    }
+
+    @Override
+    public Iterable<Position<E>> positions() {
+    	/*PositionList<Position<E>> positions = new NodePositionList<Position<E>>();
+		  	if(!isEmpty()) inorderPositions(this,positions);
+		  	return positions;*/
+    	return null;
+    }
+    
+    /*public void inorderPositions(LinkedRBinaryTree<E> tree, PositionList<Position<E>> pos){
+		  	if(tree.leftTree() != null) inorderPositions(tree.leftTree(), pos);
+		  	pos.addLast(tree.root());
+		  	if(tree.rightTree() != null) inorderPositions(tree.rightTree(), pos);
+	  	}*/
+
+}
