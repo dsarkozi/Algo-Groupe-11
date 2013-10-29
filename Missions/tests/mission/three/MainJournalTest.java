d20b5e58af4223c7967419417be5794120f06d7c
Clementine <zaninkaclem@gmail.com>
2013-10-29 08:21:33 +0100

    initialisation des tests sous JUnit

---
+++ Missions/tests/mission/three/MainJournalTest.java
@@ -0,0 +1,35 @@
+package mission.three;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import biblio.model.BookRepository;
+import biblio.model.ClientRepository;
+
+public class MainJournalTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * load the clientRepository and the bookRepository.
+	 * In this way they will be accessible for new tests
+	 */
+	private static void loadJournals() {
+		
+	}
+
+}
