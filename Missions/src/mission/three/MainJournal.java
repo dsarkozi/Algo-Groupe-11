d2c3b36c1fe6a8427512f371e1d7096cf225af1a
cmunyabarenzi <zaninkaclem@gmail.com>
2013-10-29 18:05:36 +0100

    suite implé du test de complexité

--- Missions/src/mission/three/MainJournal.java
+++ Missions/src/mission/three/MainJournal.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 public class MainJournal
 {
 	HashMap<String, Journal> dictionary;
+	static ArrayList<HashMap<String, Journal>> testDics;
+	
 	public MainJournal()
 	{
 		Journal.rankMap_init();
@@ -35,6 +37,75 @@ public class MainJournal
 		{
 			mr.addJournal(line);
 		}
+		
+		/*
+		 * Génération du matériel de tests de complexité
+		 */
+		
+		//10 dictionnaires de tailles différentes
+		for(int i= 0; i < 10; i++){
+			testDics.add(new HashMap<String, Journal>());
+		}
+		
+		//Garnir les dicos
+		String[] data;
+		Journal journal;
+		//une "fausse" revue
+		String dummyData = "Z,SINF1121SINF1121SINF1121SINF1121SINF1121,,,,";
+		dummyData.split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)");
+		Journal dummy = 
+				new Journal(dummyData.split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)")
+);
+		for(int i =0; i<testDics.size(); i++){
+			//lire un certain nombre de lignes destinées à chaque dico
+			try
+			{	
+			lines = FileManager.readSmallerFile(inputFile, 2071 * (i+1));//TODO hardcoded value!
+			}
+			catch (IOException e)
+			{
+				System.err
+				.println("Une erreur est survenue à la lecture du fichier "
+							+ inputFile);
+				return;
+			}
+			//spliter les lignes les transformer en Journal et les ajouter dans chaque dico
+			for (String line : lines)
+			{
+				data = line.split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)");
+				journal = new Journal(data);
+				testDics.get(i).put(journal.getTitle(), journal);
+
+			}
+			//rajouter le dummy à la fin de chaque dico
+			testDics.get(i).put(dummy.getTitle(), dummy);
+			
+			/*
+			 * TESTS
+			 * 
+			 */
+			
+			/*
+			 *Test de temps d'execution sur la methode get(Object key) 
+			 */
+			System.out.println(getExecTime());
+			
+		}
+
+		
+	}//fin main
+	
+	/**
+	 * Calcule le temps d'exécution moyen de la methode get 
+	 * sur chaque dictionnaire d'une liste de dicos 
+	 * @pre :
+	 * @post : un rapport est affiché dans la console
+	 * @exceptions :
+	 * @return void
+	 */
+	private static char[] getExecTime() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	/**
