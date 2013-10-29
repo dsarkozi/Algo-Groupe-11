a92d418d891263bbd6947462402da61dfe50eb80
cmunyabarenzi <zaninkaclem@gmail.com>
2013-10-29 13:19:31 +0100

    suite tests

--- Missions/tests/mission/three/MainJournalTest.java
+++ Missions/tests/mission/three/MainJournalTest.java
@@ -9,10 +9,24 @@ import org.junit.Test;
 import biblio.model.BookRepository;
 import biblio.model.ClientRepository;
 
+
 public class MainJournalTest {
+	
+	JournalMap jm1;
+	JournalMap jm2;
+	JournalMap jm3;
+	JournalMap jm4;
+	JournalMap jm5;
+	JournalMap jm6;
+	JournalMap jm7;
+	JournalMap jm8;
+	JournalMap jm9;
+	JournalMap jm10;
+
 
 	@Before
 	public void setUp() throws Exception {
+		
 	}
 
 	@After
@@ -20,16 +34,25 @@ public class MainJournalTest {
 	}
 
 	@Test
+	public void get() {
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
 	
 	/*
-	 * load the clientRepository and the bookRepository.
+	 * load the Journals.
 	 * In this way they will be accessible for new tests
 	 */
 	private static void loadJournals() {
-		
+		if(journalMap == null) {
+			journalMap = new journalMap();
+		}
+		assertTrue("No jounals in the map for testing", journalMap.getJournals().size()>0);		
 	}
 
 }
