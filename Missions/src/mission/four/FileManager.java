f05c8d5584733d2ad5cc7993d37d2de5e0b50498
David Sarkozi <dsarko@live.be>
2013-11-16 20:13:09 +0100

    New FileManager: readFile -> readLine

--- Missions/src/mission/four/FileManager.java
+++ Missions/src/mission/four/FileManager.java
@@ -1,50 +1,145 @@
 package mission.four;
 
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
 /**
  * Classe de gestion des fichiers d'entree
  * 
- * @author Henri Crombe
+ * @author David Sarkozi
  */
 
 public class FileManager
 {
 	/**
-	 * @param filename
-	 *            Le fichier a lire
-	 * @pre Le fichier pointe par filename existe et est lisible
-	 * @post Lit tout le contenu d'un fichier ligne par ligne et stocke chacune
-	 *       de ces lignes dans une ArrayList
-	 * @return Une ArrayList de String contenant les lignes du fichier passe en
-	 *         parametre, null si le fichier n'existe pas et ecrit un message
-	 *         d'erreur dans la console, ou lance une IOException si le fichier
-	 *         existe, mais n'est pas lisible.
+	 * Represente le reader du fichier d'entree
 	 */
+	private BufferedReader reader;
 
-	public static ArrayList<String> readFile(String filename)
-			throws IOException
+	/**
+	 * Represente le chemin vers le fichier d'entree
+	 */
+	private String filename;
+
+	/**
+	 * @param filename
+	 *            Le nom du fichier a lire
+	 * @pre Le fichier pointe par filename existe et est lisible
+	 * @post Cree un objet FileManager avec un reader ouvert et affecte au
+	 *       fichier d'entree en cours de lecture
+	 * @see #openFile(String)
+	 */
+	public FileManager(String filename)
 	{
+		this.openFile(filename);
+		this.filename = filename;
+	}
 
-		File file = new File(filename);
-		if (file.exists())
+	/**
+	 * Lit une ligne du fichier d'entree. Est equivalent a
+	 * {@link #readLine(false)}
+	 * 
+	 * @pre Le reader est ouvert et pret a etre lu
+	 * @post Retourne la ligne lue
+	 * @return La ligne lue
+	 * @throws IOException
+	 *             Si un evenement inattendu est survenu pendant la lecture
+	 * @see #readLine(boolean)
+	 */
+	public String readLine() throws IOException
+	{
+		return this.reader.readLine();
+	}
+
+	/**
+	 * Lit une ligne du fichier d'entree et ferme le reader si specifie
+	 * 
+	 * @param closeFile
+	 *            Si defini a true, le reader est ferme apres lecture
+	 * @pre Le reader est ouvert et pret a etre lu
+	 * @post Retourne la ligne lue et ferme le reader si closeFile est a true
+	 * @return La ligne lue
+	 * @throws IOException
+	 *             Si un evenement inattendu est survenu pendant la lecture
+	 */
+	public String readLine(boolean closeFile) throws IOException
+	{
+		String currentLine = this.reader.readLine();
+		if (closeFile)
 		{
-
-			BufferedReader bufreader = new BufferedReader(new FileReader(file));
-			ArrayList<String> content = new ArrayList<String>();
-			String currentLine = bufreader.readLine();
-			while ((currentLine = bufreader.readLine()) != null)
-			{
-				content.add(currentLine);
-			}
-			bufreader.close();
-			return content;
+			reader.close();
+			reader = null;
 		}
-		else System.err.println("Read failed : File doesn't exist");
-		return null;
+		return currentLine;
+	}
+
+	/**
+	 * Ouvre le fichier d'entree dont le chemin de this est {@link #filename}
+	 * 
+	 * @pre {@link #filename} existe et est lisible
+	 * @post {@link #reader} contient le fichier ouvert et est pret a etre lu
+	 */
+	public void openFile()
+	{
+		try
+		{
+			this.reader = new BufferedReader(new FileReader(filename));
+		}
+		catch (FileNotFoundException e)
+		{
+			System.err.println("Read failed : File doesn't exist");
+			System.exit(-1);
+		}
+	}
+
+	/**
+	 * Ouvre le fichier d'entree specifie par filename
+	 * 
+	 * @param filename
+	 *            Le fichier a ouvrir
+	 * @pre Le fichier pointe par filename existe et est lisible
+	 * @post {@link #reader} contient le fichier ouvert et est pret a etre lu,
+	 *       et {@link #filename} contient le nouveau fichier d'entree
+	 * @see #openFile()
+	 * @see #setFilename(String)
+	 */
+	public void openFile(String filename)
+	{
+		this.setFilename(filename);
+		this.openFile();
+	}
+
+	/**
+	 * Rouvre le fichier d'entree pointe par {@link #filename}
+	 * 
+	 * @pre -
+	 * @post Rouvre le fichier d'entree en le fermant prealablement si
+	 *       {@link #reader} n'est pas null
+	 * @throws IOException
+	 *             Si un evenement inattendu est survenu pendant la fermeture
+	 *             prealable du fichier d'entree
+	 * @see #openFile()
+	 */
+	public void reopenFile() throws IOException
+	{
+		if (reader != null)
+		{
+			reader.close();
+			reader = null;
+		}
+		this.openFile();
+	}
+
+	/**
+	 * @param filename
+	 *            Le chemin vers un fichier a lire
+	 * @pre -
+	 * @post Affecte filename a l'attribut de this
+	 */
+	public void setFilename(String filename)
+	{
+		this.filename = filename;
 	}
 }
