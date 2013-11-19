04c708b138ebd41fd3fd7a8e58050373554db2b5
David Sarkozi <dsarko@live.be>
2013-11-19 19:22:59 +0100

    OurBuilder -> TreeBuilder

---
+++ Missions/src/mission/four/TreeBuilder.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package mission.four;
+
+import java.util.ArrayList;
+
+/**
+ * @author Clémentine
+ * @param <T>
+ *            Decrit une Classe qui construit un Tree à partir de Journals
+ * 
+ */
+public class TreeBuilder
+{
+
+	/* the information */
+	private ArrayList<Journal> journals;
+	private Tree<Journal> tree;
+	/*
+	 * les données des champs du premier journal du tableau seront décisives
+	 * pour l'allure de l'arbre
+	 */
+	private int numFields;
+
+	/**
+	 * Construire une structure T à partir d'une liste
+	 */
+	public TreeBuilder(ArrayList<Journal> journals)
+	{
+
+		this.journals = journals;
+	}
+
+	/**
+	 * Cree un Tree destine a contenir des Journals
+	 * 
+	 * @pre journals ne contient pas de valeur null
+	 * @post
+	 * @exceptions
+	 * @return
+	 */
+	public Tree<Journal> build()
+	{
+		Journal jr;
+		tree = new Tree<Journal>(numFields);
+		// pour chaque critère
+		for (int i = 0; i < numFields; i++)
+		{
+			// ajout selon le critère i
+			tree.setCurrentPutKey(i);
+			// pour chaque journal
+			for (int j = 0; j < journals.size(); j++)
+			{
+				jr = journals.get(j);
+				tree.put(jr.getData(i), (journals.get(j)));
+			}
+		}
+		return tree;
+	}
+
+}
