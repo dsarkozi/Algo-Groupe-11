474cf612a7ac700499ea70729068664c785908f4
lacomblezl <loic.lacomblez@student.uclouvain.be>
2013-11-30 13:37:01 +0100

    Correction du commit (manquait le -a) :p

--- Missions/src/mission/five/Code.java
+++ Missions/src/mission/five/Code.java
@@ -1,5 +1,44 @@
 package mission.five;
 
-public class Code {
+import java.util.ArrayList;
 
+/** 
+ * Une classe representant les codes binaires associes aux caracteres du 
+ * @author Loic Lacomblez
+ *
+ */
+public class Code
+{
+	private ArrayList<Boolean> bits;
+	
+	/**
+	 * Cree un nouvel objet code vide
+	 */
+	public Code()
+	{
+		bits = new ArrayList<Boolean>();
+	}
+	
+	/**
+	 * Configure le (i+1)-eme bit du code a la valeur 'bit'
+	 * @pre _
+	 * @post le code a une longueur >= index+1, dont le (i+1)-eme bit a la valeur
+	 * 'bit' 
+	 */
+	public void setBit(int index, boolean bit)
+	{
+		if(index + 1 > bits.size())
+			bits.ensureCapacity(index + 1);
+		bits.add(index, bit);
+	}
+	
+	/**
+	 * Retourne le code binaire represente par l'objet
+	 * @pre _
+	 * @post _
+	 */
+	public ArrayList<Boolean> getCode()
+	{
+		return bits;
+	}
 }
--- Missions/src/mission/five/HuffmanCoder.java
+++ Missions/src/mission/five/HuffmanCoder.java
@@ -3,34 +3,58 @@ package mission.five;
 import java.util.*;
 
 /**
- * 
- * @author Benoit Sluysmans
+ * Classe englobant les structures et l'algorithme utilises par le codage de Huffman
+ * @author Benoit Sluysmans & Loic Lacomblez
  */
-public class HuffmanPriorityQueue {
+public class HuffmanCoder
+{
+	private Tree codeTree;
+
+	/**
+	 * Constructeur de la classe HuffmanCoder
+	 * @author Loic Lacomblez
+	 * @pre occurences contient une collection valide de couples caracteres--entier>0
+	 * @post _
+	 */
+	public HuffmanCoder(HashMap<Character,Integer> occurences)
+	{
+		codeTree = buildTree(occurences);
+	}
+	
+	/**
+	 * Retourne les codes lies a chaque caractere de l'arbre codeTree
+	 * @author Loic Lacomblez
+	 */
+	public HashMap<Character, Code> generateCodes()
+	{
+		// TODO
+	}
 
 	/**
 	 * 
-	 * @pre frequences.length > 0 && frequences.length == lettres.length
+	 * @pre _
 	 * @post un arbre binaire representant l'arbre utilise dans l'algorithme de Huffman
 	 */
-	 public Tree buildTree(int[] frequences, char[] lettres) 
-	 {
-		    //Creation de la priorityQueue de Tree
-	        PriorityQueue<Tree> tas = new PriorityQueue<Tree>();
-	        for (int i = 0; i < frequences.length; i++)
-	        {
-	            if (frequences[i] > 0)
-	            	tas.add(new Leaf(frequences[i], (char)i));
-	        }
-	        
-	        //boucle jusqu'a ce qu'il ne reste qu'un seul Tree dans la queue
-	        while (tas.size() > 1) 
-	        {
-	            Tree gauche = tas.poll();
-	            Tree droit = tas.poll();
-	            tas.add(new Node(gauche, droit));
-	        }
-	        
-	        return tas.poll();
-	    }
+	private Tree buildTree(HashMap<Character,Integer> occurences) 
+	{
+		//Creation de la priorityQueue de Tree
+		PriorityQueue<Tree> tas = new PriorityQueue<Tree>();
+		//Recuperation des occurences des caracteres
+		Set<Character>chars = occurences.keySet();
+
+		for(char letter:chars)
+		{
+			tas.add(new Leaf(occurences.get(letter), letter));
+		}
+
+		// boucle jusqu'a ce qu'il ne reste qu'un seul Tree dans la queue
+		while (tas.size() > 1) 
+		{
+			Tree gauche = tas.poll();
+			Tree droit = tas.poll();
+			tas.add(new Node(gauche, droit));
+		}
+
+		return tas.poll();
+	}
 }
