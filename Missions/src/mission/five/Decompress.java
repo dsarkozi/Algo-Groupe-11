23b40483c073fd1ec413e5cd9f755e2cc44b9389
David Sarkozi <dsarko@live.be>
2013-11-30 19:56:17 +0100

    Compress/Decompress skeleton

--- Missions/src/mission/five/Decompress.java
+++ Missions/src/mission/five/Decompress.java
@@ -8,15 +8,39 @@ package mission.five;
  */
 public class Decompress
 {
+	private String inputFile;
+	private String outputFile;
+
+	/**
+	 * Constructor of the class.
+	 * 
+	 * @param inputFile
+	 *            The input file to compress.
+	 * @param outputFile
+	 *            The output file that will contain the compressed input file.
+	 */
+	public Decompress(String inputFile, String outputFile)
+	{
+		this.inputFile = inputFile;
+		this.outputFile = outputFile;
+	}
+
 	/**
 	 * 
 	 * @param args
 	 *            Contains the arguments of the program (the input filename at
 	 *            index 0, the output filename at index 1).
 	 * @pre args contains valid data.
-	 * @post Decompresses the input file and returns the result in the output file.
+	 * @post Decompresses the input file and returns the result in the output
+	 *       file.
 	 */
 	public static void main(String[] args)
 	{
+		if (args.length < 1 || args.length > 2)
+		{
+			System.err.println("Invalid number of arguments.");
+			return;
+		}
+		Decompress unzip = new Decompress(args[0], args[1]);
 	}
 }
