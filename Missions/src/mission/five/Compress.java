1183cb273091baf14df01bce061f8ce2074a22ed
HenriCrombe <henri.crombe@student.uclouvain.be>
2013-12-01 16:18:24 +0100

    Working on huff_compress

--- Missions/src/mission/five/Compress.java
+++ Missions/src/mission/five/Compress.java
@@ -1,4 +1,8 @@
 package mission.five;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
 /**
  * Class taking care of file compression.
@@ -24,6 +28,57 @@ public class Compress
 		this.inputFile = inputFile;
 		this.outputFile = outputFile;
 	}
+	
+	
+	public void huff_compress(Compress zip){
+		
+		File in = new File(zip.inputFile);
+		File out = new File(zip.outputFile);
+		// On place tout le contenu du fichier dans un String, histoire de pouvori travailler char par char
+		String text_tocompress = FileToString(in);
+		char[] textc_tocompress = text_tocompress.toCharArray();
+		
+		HashMap<Character,Integer> occurences = new HashMap<Character,Integer>();
+		
+		for(Character c : textc_tocompress){
+				// On vérifie que le caractère courrant n'existe pas deja dans le HashMap
+				boolean alreadyInHash = occurences.containsKey(c);
+				if(alreadyInHash){
+						// On incremente l'occurence du caractere c
+						occurences.put(c, occurences.get(c) + 1);
+				}
+				else{	
+						// Nouvelle occurence du caractere c
+						occurences.put(c, 1);	
+				}
+			
+		}
+		
+		HuffmanCoder hc = new HuffmanCoder(occurences);
+		
+		
+	}
+	
+	public String FileToString(File in){
+		
+		if (in.exists())
+		{
+
+			BufferedReader bufreader = new BufferedReader(new FileReader(file));
+			String text = "";
+			String currentLine = null;
+			while ((currentLine = bufreader.readLine()) != null)
+			{
+				text += currentLine;
+			}
+			bufreader.close();
+			return text;
+		}
+		else System.out.println("Read failed : File doesn't exist");
+		return null;
+	}
+		
+		
 
 	/**
 	 * 
@@ -42,5 +97,10 @@ public class Compress
 			return;
 		}
 		Compress zip = new Compress(args[0], args[1]);
+		
+
+		
 	}
+	
+	
 }
