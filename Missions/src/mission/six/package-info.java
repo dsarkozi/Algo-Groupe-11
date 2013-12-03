cb995ad990de295b94c0d1004dc0463ddb1c82d4
David Sarkozi <dsarko@live.be>
2013-12-04 00:06:46 +0100

    Mission 6 init

---
+++ Missions/src/mission/six/package-info.java
@@ -0,0 +1,66 @@
+/**
+ * Mission 6 - 
+ * 
+ * <ul>
+ * <li>Clementine Munyabarenzi</li>
+ * <ul>
+ * <li></li>
+ * </ul>
+ * <ul>
+ * 
+ * </ul>
+ * <li>Loic Lacomblez</li>
+ * <ul>
+ * <li></li>
+ * </ul>
+ * <ul>
+ * 
+ * </ul>
+ * <li>Alaaedine Chatri</li>
+ * <ul>
+ * <li></li>
+ * </ul>
+ * <ul>
+ * 
+ * </ul>
+ * <li>Benoit Sluysmans</li>
+ * <ul>
+ * <li></li>
+ * </ul>
+ * <ul>
+ * 
+ * </ul>
+ * <li>David Sarkozi</li>
+ * <ul>
+ * <li>Javadoc</li>
+ * </ul>
+ * <ul>
+ * 
+ * </ul>
+ * <li>Henri Crombe</li>
+ * <ul>
+ * <li></li>
+ * </ul>
+ * <ul>
+ * 
+ * </ul>
+ * <li>Julien De Maeyer</li>
+ * <ul>
+ * <li></li>
+ * </ul>
+ * <ul>
+ * 
+ * </ul>
+ * <li>Nicolas Marchand</li>
+ * <ul>
+ * <li></li>
+ * </ul>
+ * <ul>
+ * 
+ * </ul>
+ * </ul>
+ * 
+ * @author Group 11
+ * 
+ */
+package mission.six;
